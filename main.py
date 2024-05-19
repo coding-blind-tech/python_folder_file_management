45c9d28cbac336e58b7be7147d582da7c5ca5595
kblackwelder <kblackwelder08@gmail.com>
2024-05-19 19:01:15 -0400

    Commented out the delete of directory for now.

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ directory_name = "my_directory"
 if not os.path.exists(directory_name):
     os.mkdir(directory_name)
 
+
+
+
+
+
 # Delete empty directory
-if os.path.exists(directory_name):
+# if os.path.exists(directory_name):
     # os.rmdir(directory_name)
-    shutil.rmtree(directory_name)
+    # shutil.rmtree(directory_name)
 
